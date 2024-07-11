847a34ea0c85817b9681aa743ec01511be1a36ed
theo <98850030+theo-retical@users.noreply.github.com>
2024-07-11 10:43:33 +0100

    Create DeleteContactCommand.java

---
+++ week 2/assignment 2.3/DeleteContactCommand.java
@@ -0,0 +1,25 @@
+package com.example.sharingapp;
+
+import android.content.Context;
+
+/**
+ * Command to delete contact
+ */
+
+public class DeleteContactCommand extends Command {
+
+  private ContactList contact_list;
+  private Contact contact;
+  private Context context;
+
+  public AddContactCommand(ContactList contact_list, Contact contact, Context context) {
+    this.contact_list = contact_list;
+    this.contact = contact;
+    this.context = context;
+  }
+
+  public void execute() {
+    contact_list.deleteContact(contact);
+    setIsExecuted(contact_list.saveContacts(context));
+  }
+}
