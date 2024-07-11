e4ac319cd5b29699d90b16318a4b512306f09915
theo <98850030+theo-retical@users.noreply.github.com>
2024-07-11 10:46:41 +0100

    Create observer_starter.java

---
+++ week 2/observer pattern/observer_starter.java
@@ -0,0 +1,48 @@
+Subject.java
+
+public interface Subject {
+
+
+
+
+}
+
+
+
+
+Channel.java
+
+public class Channel implements Subject {
+  
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+Observer.java
+
+public interface Observer {
+	public void update(String status);
+}
+
+
+
+
+
+Follower.java
+
+public class Follower implements Observer {
+
+
+
+
+
+
+
+}
