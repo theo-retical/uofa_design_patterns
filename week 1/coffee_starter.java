0ed497c0d2e62f3062b2a744921740acb59d1187
theo <98850030+theo-retical@users.noreply.github.com>
2024-07-11 10:35:44 +0100

    Create coffee_starter.java

---
+++ week 1/coffee_starter.java
@@ -0,0 +1,34 @@
+CoffeeMachineInterface.java
+
+public interface CoffeeMachineInterface {
+
+
+
+}
+
+
+OldCoffeeMachine.java
+
+public class OldCoffeeMachine {
+
+
+
+
+
+}
+
+
+
+
+
+
+CoffeeTouchscreenAdapter.java
+
+public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface {
+
+
+
+
+
+
+}
